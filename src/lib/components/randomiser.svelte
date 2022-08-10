bea6c9c05e3d2788b9acb63b2aeef78073678369
AndrewKraevskii <75577902+AndrewKraevskii@users.noreply.github.com>
2022-08-10 19:08:58 +0300

    update randomiser to use less data

--- src/lib/components/randomiser.svelte
+++ src/lib/components/randomiser.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-	import type { Card, ChampionInfo } from '$api/getchampioninfo';
+	import type { Card, ChampionInfo } from '$lib/paladins_api/getchampioninfo';
 	import { choice, randrange, sample } from '$lib/utils';
 
 	export let champions: ChampionInfo[];
 	let champion: {
 		name: string;
 		talent: string;
-		cards: Card[];
-		nums: number[];
+        cards: {
+            name: string;
+            level: number
+        }[];
 		items: string[];
 	} | null = null;
 
@@ -28,44 +30,93 @@
 			c.cards.filter((card) => card.rarity === 'Common'),
 			5
 		);
-        let items = [
-				'Освещение',
-				'Устойчивость',
-				'Страж',
-				'Укрытие',
-				'Гибкость',
-				'Мастер Езды',
-				'Боевой Дух',
-				'Хронос',
-				'Целебные Убийства',
-				'Вырывание Жизни',
-				'Омоложение',
-				'Ветеран',
-				'Бульдозер',
-				'Ловкие пальцы',
-				'Смертоносность',
-				'Крушитель'
-			];
-        if (["Бак","Лилит","VII","Азаан","Андроксус","Атлас","Бетти Ля Бомба","Вату","Виктро","Вивиан","Вилло","Вора","Гровер","Грок","Дженос","Дзин","Дредж","Дрогоз","Иви","Имани","Инара","Инь","Ио","Кинесса","Кога","Король Бомб","Кэсси","Лекс","Лиан","Мейв","Моджи","Мэл Дэмба","Октавия","Пип","Раум","Рей","Саати","Серис","Скай","Стрикс","Тайра","Талус","Тибериус","Фурия","Ша Линь"].includes(c.api_information.Name)) {
-            items = items.filter(name => name !== "Страж")
-        }
-        if (["Гровер","Азаан","Имани","Моджи","Терминус","Ша Линь","Ягорат"].includes(c.api_information.Name)) {
-            items = items.filter(name => name !== "Ловкие Пальцы")
-        }
-        if (["Ягорат"].includes(c.api_information.Name)) {
-            items = items.filter(name => name !== "Гибкость")
-            items = items.filter(name => name !== "Мастер Езды")
-        }
-		
-        items = sample(
-			items,
-			4
-		);
+		let items = [
+			'Освещение',
+			'Устойчивость',
+			'Страж',
+			'Укрытие',
+			'Гибкость',
+			'Мастер Езды',
+			'Боевой Дух',
+			'Хронос',
+			'Целебные Убийства',
+			'Вырывание Жизни',
+			'Омоложение',
+			'Ветеран',
+			'Бульдозер',
+			'Ловкие пальцы',
+			'Смертоносность',
+			'Крушитель'
+		];
+		if (
+			[
+				'Бак',
+				'Лилит',
+				'VII',
+				'Азаан',
+				'Андроксус',
+				'Атлас',
+				'Бетти Ля Бомба',
+				'Вату',
+				'Виктро',
+				'Вивиан',
+				'Вилло',
+				'Вора',
+				'Гровер',
+				'Грок',
+				'Дженос',
+				'Дзин',
+				'Дредж',
+				'Дрогоз',
+				'Иви',
+				'Имани',
+				'Инара',
+				'Инь',
+				'Ио',
+				'Кинесса',
+				'Кога',
+				'Король Бомб',
+				'Кэсси',
+				'Лекс',
+				'Лиан',
+				'Мейв',
+				'Моджи',
+				'Мэл Дэмба',
+				'Октавия',
+				'Пип',
+				'Раум',
+				'Рей',
+				'Саати',
+				'Серис',
+				'Скай',
+				'Стрикс',
+				'Тайра',
+				'Талус',
+				'Тибериус',
+				'Фурия',
+				'Ша Линь'
+			].includes(c.api_information.Name)
+		) {
+			items = items.filter((name) => name !== 'Страж');
+		}
+		if (
+			['Гровер', 'Азаан', 'Имани', 'Моджи', 'Терминус', 'Ша Линь', 'Ягорат'].includes(
+				c.api_information.Name
+			)
+		) {
+			items = items.filter((name) => name !== 'Ловкие Пальцы');
+		}
+		if (['Ягорат'].includes(c.api_information.Name)) {
+			items = items.filter((name) => name !== 'Гибкость');
+			items = items.filter((name) => name !== 'Мастер Езды');
+		}
+
+		items = sample(items, 4);
+        const nums = getRandomNumbers(5, 1, 5);
 		champion = {
 			name,
 			talent: talant.card_name,
-			cards,
-			nums: getRandomNumbers(5, 1, 5),
+			cards: cards.map((card, index)=>{return {name: card.card_name, level: nums[index]};}),
 			items
 		};
 	}
@@ -75,8 +126,8 @@
 	Name: {champion.name}<br />
 	Talent: {champion.talent}<br />
 	Cards:
-	{#each champion.cards as card, index}
-		{card.card_name}:{champion.nums[index]},
+	{#each champion.cards as card}
+		{card.name}:{card.level},
 	{/each}<br />
 	Items:
 	{#each champion.items as item, index}
