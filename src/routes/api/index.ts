a54780d218a410616c4b6d97d9d482d51580f78c
Andrew <andrew.kraevskii@gmail.com>
2022-08-10 11:42:35 +0300

    try to fix loading timeout

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,12 +1,20 @@
-import { getChampionsInfo } from "$api/getchampioninfo"
+import { getChampionsInfo, type ChampionInfo } from "$api/getchampioninfo"
 
+let data: ChampionInfo[] | null = null;
 
 export async function GET() {
+    if (data === null) 
+    {
+        data = await getChampionsInfo();
+        setTimeout(() => {
+
+        }, 3600 * 1000)
+    }
     return {
         status: 200,
         headers: {
             'cache-control': 'public, max-age=3600'
         },
-        body: await getChampionsInfo()
+        body: data
     }
 }
