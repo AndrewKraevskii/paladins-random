a86937491e8d1d1b0ada985c3fec08adcd4bd09c
Andrew <andrew.kraevskii@gmail.com>
2022-08-10 06:51:46 +0300

    items usage correction

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -4,6 +4,9 @@ import { getChampionsInfo } from "$api/getchampioninfo"
 export async function GET() {
     return {
         status: 200,
+        headers: {
+            'cache-control': 'public, max-age=3600'
+        },
         body: await getChampionsInfo()
     }
 }
